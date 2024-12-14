b1d35a6e44d3475a8692db0dce165cde2c599806
Robert Vilcu <robertvilcu89@gmail.com>
2024-12-14 15:46:20 +0000

    chore: refactor configurations for both vite and router

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,12 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+    // Config options...
+    // Server-side render by default, to enable SPA mode set this to `false`
+    ssr: false,
+
+	// Use this to pre render any routes at build time
+	// async prerender() {
+	// 	return ["/", "/about", "/contact"];
+	// },
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,59 @@
 import { reactRouter } from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
-import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
+
+import { defineConfig } from "vite";
+
+// Using any is BAD, but it's only for development
+const devConfig = {
+    server: {
+        port: 3000,
+        cors: true,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5001',
+                changeOrigin: true,
+                secure: false,
+                ws: true,
+                configure: (proxy: any, _options: any) => {
+                    proxy.on('error', (err: any, _req: any, _res: any) => {
+                        console.log('proxy error', err);
+                    });
+                    proxy.on('proxyReq', (proxyReq: any, req: any, _res: any) => {
+                        console.log('Sending Request to the Target:', req.method, req.url);
+                    });
+                    proxy.on('proxyRes', (proxyRes: any, req: any, _res: any) => {
+                        console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+                    });
+                },
+            }
+        }
+    },
+};
+
+const buildConfig = {
+	build: {
+		outDir: path.resolve(__dirname, 'build'),
+	},
+};
+
+const config = process.env.NODE_ENV === 'production' ? buildConfig : devConfig;	
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
+	css: {
+		postcss: {
+			plugins: [tailwindcss, autoprefixer],
+		},
+	},
+	plugins: [reactRouter(), tsconfigPaths()],
+
+	resolve: {
+        alias: {
+            "~": path.resolve(__dirname, "./app"),
+        },
     },
-  },
-  plugins: [reactRouter(), tsconfigPaths()],
+
+	...config,
 });
