df933454784b6aa92f1a46de8311b8e282102da4
Robert Vilcu <robertvilcu89@gmail.com>
2024-12-14 16:16:52 +0000

    chore: changed auth base url + package.json

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,4 +1,4 @@
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-    baseURL: import.meta.env.VITE_API_URL,
-})
+	baseURL: import.meta.env.NODE_ENV === "development" ? import.meta.env.VITE_API_URL : import.meta.env.VITE_RAILWAY_PUBLIC_API_URL,
+});
