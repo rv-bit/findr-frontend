8b2a1e6dc9d0a05b6195a4a2b3e9a471bf159a8c
Robert Vilcu <robertvilcu89@gmail.com>
2025-01-12 16:55:23 +0000

    feat: added plugins

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,4 +1,10 @@
 import { createAuthClient } from "better-auth/react"
+import { twoFactorClient, usernameClient } from "better-auth/client/plugins"
+
 export const authClient = createAuthClient({
     baseURL: import.meta.env.VITE_API_URL,
+    plugins: [
+        twoFactorClient(),
+        usernameClient()
+    ]
 })
