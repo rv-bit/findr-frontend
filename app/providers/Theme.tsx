d1338eb763913aabd737b57a16684b770bcae45b
Robert Vilcu <robertvilcu89@gmail.com>
2025-01-15 18:04:33 +0000

    feat: made theme cookie to last one year

--- app/providers/Theme.tsx
+++ app/providers/Theme.tsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
 const THEME_COOKIE_NAME = "theme:state"
-const THEME_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
+const THEME_COOKIE_MAX_AGE = 60 * 60 * 24 * 7 * 365; // 1 year
 
 interface ThemeProvider {
     theme: 'dark' | 'light';
