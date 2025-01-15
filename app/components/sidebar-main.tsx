29c7f77f3ab97ea2e4742a493b6aa27ba9b9be6e
Robert Vilcu <robertvilcu89@gmail.com>
2025-01-15 14:18:09 +0000

    chore: refactor sidebar

--- app/components/sidebar-main.tsx
+++ app/components/sidebar-main.tsx
@@ -1,58 +1,182 @@
-import { authClient } from "~/lib/auth";
+import React from "react";
+import { Link, useSearchParams } from "react-router";
+import { type LucideIcon, ChevronDown, CircleArrowOutUpRight, Home } from "lucide-react";
 
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
-import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "~/components/ui/sidebar";
+import {
+	Collapsible,
+	CollapsibleContent,
+	CollapsibleTrigger,
+} from "~/components/ui/collapsible"
+
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarMenuSub,
+	SidebarMenuSubButton,
+	SidebarMenuSubItem,
+} from "~/components/ui/sidebar"
 
 import { NavUser } from "./sidebar-user";
 import { NavFooter } from "./sidebar-footer";
 
-const items = [
+interface Actions {
+	title: string;
+	url: string;
+	icon?: LucideIcon;
+	isActive?: boolean;
+	isCollapsible?: boolean;
+	items?: Actions[];
+}
+
+const actions: Actions[] = [
 	{
-		title: "Home",
+		title: "Main",
 		url: "#",
-		icon: Home,
+		isCollapsible: false,
+		items: [
+			{
+				title: "Home",
+				url: "#",
+				icon: Home,
+			},
+			{
+				title: "Popular",
+				url: "#",
+				icon: CircleArrowOutUpRight,
+			},
+			{
+				title: "Explore",
+				url: "#",
+				icon: Home,
+			},
+		]
 	},
 	{
 		title: "Inbox",
 		url: "#",
-		icon: Inbox,
-	},
-	{
-		title: "Calendar",
-		url: "#",
-		icon: Calendar,
-	},
-	{
-		title: "Search",
-		url: "#",
-		icon: Search,
+		isActive: true,
+		isCollapsible: true,
+		items: [
+			{
+				title: "Introduction",
+				url: "#",
+			},
+			{
+				title: "Get Started",
+				url: "#",
+			},
+			{
+				title: "Tutorials",
+				url: "#",
+			},
+			{
+				title: "Changelog",
+				url: "#",
+			},
+		],
 	},
 	{
 		title: "Settings",
 		url: "#",
-		icon: Settings,
+		isActive: true,
+		isCollapsible: true,
+		items: [
+			{
+				title: "General",
+				url: "#",
+			},
+			{
+				title: "Team",
+				url: "#",
+			},
+			{
+				title: "Billing",
+				url: "#",
+			},
+			{
+				title: "Limits",
+				url: "#",
+			},
+		],
 	},
 ];
 
-export function AppSidebar() {
-	const { data: session, isPending, error } = authClient.useSession();
+export function SidebarActions() {
+	const [searchParams, setSearchParams] = useSearchParams();
 
 	return (
-		<Sidebar variant="inset" collapsible="icon">
+		<Sidebar variant="sidebar" collapsible="icon">
 			<SidebarContent>
 				<SidebarGroup>
 					<SidebarGroupContent>
 						<SidebarMenu>
-							{items.map((item) => (
-								<SidebarMenuItem key={item.title}>
-									<SidebarMenuButton asChild>
-										<a href={item.url}>
-											<item.icon />
-											<span>{item.title}</span>
-										</a>
-									</SidebarMenuButton>
-								</SidebarMenuItem>
+							{actions.map((item) => (
+								!item.isCollapsible && (
+									item.items?.map((action) => (
+										<SidebarMenuItem key={action.title}>
+											<Link to={action.url}>
+												<SidebarMenuButton tooltip={action.title} size={"lg"} className="h-10">
+													{action.icon && <action.icon size={32} />}
+													<span>{action.title}</span>
+												</SidebarMenuButton>
+											</Link>
+										</SidebarMenuItem>
+									))
+								)
 							))}
+						</SidebarMenu>
+						<hr className="w-100 my-3 border-sidebar-border"></hr>
+						<SidebarMenu className="gap-0">
+							{actions.map((item, index) => {
+								return (
+									item.isCollapsible && (
+										<React.Fragment key={index}>
+											<Collapsible
+												asChild
+												defaultOpen={item.isActive}
+												className="group/collapsible"
+											>
+												<SidebarMenuItem>
+													<CollapsibleTrigger asChild>
+														<SidebarMenuButton
+															tooltip={item.title}
+															size={"lg"}
+															className="h-10"
+														>
+															{item.icon && <item.icon size={32} />}
+															<span className="uppercase dark:text-neutral-300">{item.title}</span>
+															<ChevronDown className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-180" />
+														</SidebarMenuButton>
+													</CollapsibleTrigger>
+													<CollapsibleContent>
+														<SidebarMenuSub>
+															{item.items?.map((subItem, subIndex) => (
+																<SidebarMenuSubItem key={subIndex}>
+																	<SidebarMenuSubButton asChild size="lg">
+																		<a href={subItem.url}>
+																			<span>{subItem.title}</span>
+																		</a>
+																	</SidebarMenuSubButton>
+																</SidebarMenuSubItem>
+															))}
+														</SidebarMenuSub>
+													</CollapsibleContent>
+												</SidebarMenuItem>
+											</Collapsible>
+
+											{index < actions.length - 1 && (
+												<hr className="w-100 my-3 border-sidebar-border"></hr>
+											)}
+										</React.Fragment>
+									)
+								);
+							})}
 						</SidebarMenu>
 					</SidebarGroupContent>
 				</SidebarGroup>
@@ -62,6 +186,6 @@ export function AppSidebar() {
 				<NavFooter />
 				<NavUser />
 			</SidebarFooter>
-		</Sidebar>
+		</Sidebar >
 	);
 }
--- app/components/ui/sidebar.tsx
+++ app/components/ui/sidebar.tsx
@@ -208,7 +208,7 @@ const Sidebar = React.forwardRef<
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
-            className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
+            className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden border-r border-sidebar-border"
             style={
               {
                 "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
@@ -251,7 +251,7 @@ const Sidebar = React.forwardRef<
             // Adjust the padding for floating and inset variants.
             variant === "floating" || variant === "inset"
               ? "p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)_+_theme(spacing.4)_+2px)]"
-              : "group-data-[collapsible=icon]:w-[--sidebar-width-icon] group-data-[side=left]:border-r group-data-[side=right]:border-l",
+              : "group-data-[collapsible=icon]:w-[--sidebar-width-icon] group-data-[side=left]:border-r group-data-[side=right]:border-l border-sidebar-border",
             className
           )}
           {...props}
@@ -332,7 +332,7 @@ const SidebarInset = React.forwardRef<
     <main
       ref={ref}
       className={cn(
-        "relative flex min-h-svh flex-1 flex-col bg-white dark:bg-primary",
+        "relative min-h-svh flex-1 bg-white dark:bg-primary",
         "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         className
       )}
@@ -522,7 +522,7 @@ const SidebarMenuItem = React.forwardRef<
 SidebarMenuItem.displayName = "SidebarMenuItem"
 
 const sidebarMenuButtonVariants = cva(
-  "peer/menu-button flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm outline-none ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-2 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0",
+  "peer/menu-button flex w-full items-center gap-2 overflow-hidden rounded-md p-2 px-3 text-left text-sm outline-none ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-2 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0",
   {
     variants: {
       variant: {
@@ -533,7 +533,7 @@ const sidebarMenuButtonVariants = cva(
       size: {
         default: "h-8 text-sm",
         sm: "h-7 text-xs",
-        lg: "h-12 text-sm group-data-[collapsible=icon]:!p-0",
+        lg: "h-12 text-md group-data-[collapsible=icon]:!p-0",
       },
     },
     defaultVariants: {
@@ -700,7 +700,7 @@ const SidebarMenuSub = React.forwardRef<
     ref={ref}
     data-sidebar="menu-sub"
     className={cn(
-      "mx-3.5 flex min-w-0 translate-x-px flex-col gap-1 border-l border-sidebar-border px-2.5 py-0.5",
+      "flex min-w-0 translate-x-px flex-col gap-1",
       "group-data-[collapsible=icon]:hidden",
       className
     )}
@@ -719,7 +719,7 @@ const SidebarMenuSubButton = React.forwardRef<
   HTMLAnchorElement,
   React.ComponentProps<"a"> & {
     asChild?: boolean
-    size?: "sm" | "md"
+    size?: "sm" | "md" | "lg"
     isActive?: boolean
   }
 >(({ asChild = false, size = "md", isActive, className, ...props }, ref) => {
@@ -732,10 +732,11 @@ const SidebarMenuSubButton = React.forwardRef<
       data-size={size}
       data-active={isActive}
       className={cn(
-        "flex h-7 min-w-0 -translate-x-px items-center gap-2 overflow-hidden rounded-md px-2 text-sidebar-foreground outline-none ring-sidebar-ring hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0 [&>svg]:text-sidebar-accent-foreground",
+        "flex h-7 min-w-0 -translate-x-px items-center gap-2 overflow-hidden rounded-md px-5 text-sidebar-foreground outline-none ring-sidebar-ring hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0 [&>svg]:text-sidebar-accent-foreground",
         "data-[active=true]:bg-sidebar-accent data-[active=true]:text-sidebar-accent-foreground",
         size === "sm" && "text-xs",
         size === "md" && "text-sm",
+        size === "lg" && "text-md h-10",
         "group-data-[collapsible=icon]:hidden",
         className
       )}
