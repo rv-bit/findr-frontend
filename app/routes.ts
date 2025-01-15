ddb8dcaaa21b261577ee9ff007c4b77354ad0849
Robert Vilcu <robertvilcu89@gmail.com>
2025-01-15 00:20:10 +0000

    chore: changed route architecture

--- app/routes.ts
+++ app/routes.ts
@@ -3,10 +3,8 @@ import { type RouteConfig, index, route, layout } from "@react-router/dev/routes
 export default [
 	index("routes/home.tsx"), // This is the default route
 
-	layout("./routes/auth/layout.tsx", [
-		route("login", "./routes/auth/login.tsx"),
-		route("register", "./routes/auth/register.tsx"),
-	]),
+	route("login", "./routes/auth/login.tsx"),
+	route("onboard", "./routes/auth/onboarding.tsx"),
 
 	route("dashboard", "routes/dashboard.tsx"),
 ] satisfies RouteConfig;
