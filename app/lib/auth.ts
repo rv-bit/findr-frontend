7b0a559580a1aae04ff33e0a3df731d6a860acaf
Robert Vilcu <robertvilcu89@gmail.com>
2025-01-19 19:11:49 +0000

    chore: remove logs

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,9 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 import { twoFactorClient, usernameClient } from "better-auth/client/plugins"
 
-console.log(import.meta.env.MODE)
-console.log(import.meta.env.VITE_API_URL)    
-
 export const authClient = createAuthClient({
     baseURL: import.meta.env.VITE_API_URL,
     plugins: [
