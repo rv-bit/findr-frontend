ed0632cadd7bd4d17c7f7ceb9a21e6128f8b1ef7
Robert Vilcu <robertvilcu89@gmail.com>
2025-01-19 15:16:04 +0000

    chore: changed base url

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,8 +1,9 @@
 import { createAuthClient } from "better-auth/react"
 import { twoFactorClient, usernameClient } from "better-auth/client/plugins"
 
+const baseURL = import.meta.env.DEV ? import.meta.env.VITE_API_URL + "/api/auth/" : import.meta.env.VITE_RAILWAY_PUBLIC_API_URL + "/api/auth/";
 export const authClient = createAuthClient({
-    baseURL: import.meta.env.VITE_RAILWAY_PUBLIC_API_URL,
+    baseURL: baseURL,
     plugins: [
         twoFactorClient(),
         usernameClient()
