e0daa0fd544810b44fa04262e748c0849de96a70
Robert Vilcu <robertvilcu89@gmail.com>
2025-01-19 15:30:43 +0000

    chore: add some logs

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -2,12 +2,10 @@ import { createAuthClient } from "better-auth/react"
 import { twoFactorClient, usernameClient } from "better-auth/client/plugins"
 
 console.log(import.meta.env.MODE)
-console.log(import.meta.env.VITE_API_URL)
-console.log(import.meta.env.VITE_RAILWAY_PUBLIC_API_URL)    
+console.log(import.meta.env.VITE_API_URL)    
 
-const baseURL = import.meta.env.DEV ? import.meta.env.VITE_API_URL + "/api/auth/" : import.meta.env.VITE_RAILWAY_PUBLIC_API_URL + "/api/auth/";
 export const authClient = createAuthClient({
-    baseURL: baseURL,
+    baseURL: import.meta.env.VITE_API_URL,
     plugins: [
         twoFactorClient(),
         usernameClient()
