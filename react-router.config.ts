26d1e061e86792bb85f36f061aca6d68a7eb26b3
Robert Vilcu <robertvilcu89@gmail.com>
2025-01-19 15:33:24 +0000

    chore: changed router config

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@react-router/dev/config";
 export default {
 	// Config options...
 	// Server-side render by default, to enable SPA mode set this to `false`
-	ssr: true,
+	ssr: false,
 
 	// Use this to pre render any routes at build time
 	async prerender() {
