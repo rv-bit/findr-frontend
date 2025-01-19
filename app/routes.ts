5fbfbcd9876f2c14817c5958bf1c0ca4514222e5
Robert Vilcu <robertvilcu89@gmail.com>
2025-01-19 19:18:53 +0000

    chore:  changed routing

--- app/routes.ts
+++ app/routes.ts
@@ -1,11 +1,14 @@
-import { type RouteConfig, index, route, layout } from "@react-router/dev/routes";
+import { type RouteConfig, index, route, prefix } from "@react-router/dev/routes";
 
 export default [
 	index("routes/home.tsx"),
 	route("legal", "routes/legal.tsx"),
 
-	route("login", "./routes/auth/login.tsx"),
-	route("onboard", "./routes/auth/onboard.tsx"),
+	...prefix("auth", [
+		index("./routes/auth/login.tsx"), // This is the default route which will be the login page
+		route("onboard", "./routes/auth/onboard.tsx"),
+		route("forgot-password", "./routes/auth/forgot-password.tsx"),
+	]),
 
 	route("dashboard", "routes/dashboard.tsx"),
 ] satisfies RouteConfig;
