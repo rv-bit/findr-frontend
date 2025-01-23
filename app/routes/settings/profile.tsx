5624623d7da51c58a9f89426fb63d7de8e3aa7e6
Robert Vilcu <robertvilcu89@gmail.com>
2025-01-23 21:52:58 +0000

    feat: initiated profile route

---
+++ app/routes/settings/profile.tsx
@@ -0,0 +1,9 @@
+import type { Route } from "./+types/profile";
+
+export default function Profile() {
+	return (
+		<div className="flex h-full w-full flex-col items-start justify-center gap-2">
+			<h1>Profile settings</h1>
+		</div>
+	);
+}
