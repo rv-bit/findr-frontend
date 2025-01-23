0bef124a6815bca3eb26b8fe1b6235298fbe1077
Robert Vilcu <robertvilcu89@gmail.com>
2025-01-23 19:41:19 +0000

    chore: refactor settings routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,4 @@
-import { type RouteConfig, index, route, prefix } from "@react-router/dev/routes";
+import { type RouteConfig, index, route, prefix, layout } from "@react-router/dev/routes";
 
 export default [
 	index("routes/index.tsx"), // This is the default route which will be the home page
@@ -11,8 +11,10 @@ export default [
 	]),
 
 	...prefix("settings", [
-		index("./routes/settings/index.tsx"), // This is the default route which will be the settings page
-		// route("profile", "./routes/settings/profile.tsx"),
-		// route("account", "./routes/settings/account.tsx"),
+		layout("./routes/settings/_layout.tsx", [
+			index("./routes/settings/account.tsx", {id: "index-settings"}), // This is the default route which will be the account page
+			route("account", "./routes/settings/account.tsx"),
+			route("profile", "./routes/settings/profile.tsx"),
+		]),
 	]),
 ] satisfies RouteConfig;
