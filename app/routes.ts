1d5905e3ce194a21671b8d4117fc507c16125bbf
Robert Vilcu <robertvilcu89@gmail.com>
2025-01-26 19:58:35 +0000

    chore: refactor settings routes + new verify email route

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,4 @@
-import { type RouteConfig, index, route, prefix, layout } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, prefix, route } from "@react-router/dev/routes";
 
 export default [
 	index("routes/index.tsx"), // This is the default route which will be the home page
@@ -8,12 +8,13 @@ export default [
 		index("./routes/auth/login.tsx"), // This is the default route which will be the login page
 		route("onboard", "./routes/auth/onboard.tsx"),
 		route("forgot-password", "./routes/auth/forgot-password.tsx"),
+		route("verify-email", "./routes/auth/verify-email.tsx"),
 	]),
 
 	...prefix("settings", [
 		layout("./routes/settings/_layout.tsx", [
-			index("./routes/settings/account.tsx", {id: "index-settings"}), // This is the default route which will be the account page
-			route("account", "./routes/settings/account.tsx"),
+			index("./routes/settings/account/index.tsx", { id: "index-settings" }), // This is the default route which will be the account page
+			route("account", "./routes/settings/account/index.tsx"),
 			route("profile", "./routes/settings/profile.tsx"),
 		]),
 	]),
