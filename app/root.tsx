18019c6b8a3ff8dcfacb4c57da454a6e64c79088
Robert Vilcu <robertvilcu89@gmail.com>
2025-01-31 13:25:11 +0000

    chore: removed mdx css from root

--- app/root.tsx
+++ app/root.tsx
@@ -3,7 +3,6 @@ import { isRouteErrorResponse, Links, Meta, Outlet, Scripts, ScrollRestoration, 
 
 import type { Route } from "./+types/root";
 import stylesheet from "./app.css?url";
-import mdxeditor_stylesheet from "./editor.css?url";
 
 import { QueryClientProvider } from "@tanstack/react-query";
 
@@ -39,7 +38,6 @@ export const links: Route.LinksFunction = () => [
 		href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
 	},
 	{ rel: "stylesheet", href: stylesheet },
-	{ rel: "stylesheet", href: mdxeditor_stylesheet },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
