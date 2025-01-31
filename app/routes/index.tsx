8548c53454c856bb7362c1098436abd70d778d17
Robert Vilcu <robertvilcu89@gmail.com>
2025-01-31 16:52:27 +0000

    chore: preparing index / homepage to put all posts in

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,23 +1,73 @@
-import React, { useEffect } from "react";
+import type { Route } from "./+types/index";
 
 import { useInfiniteQuery } from "@tanstack/react-query";
+import React, { useEffect } from "react";
 
-export default function Index() {
+// export async function loader({ params }: Route.LoaderArgs) {
+// 	const data = await queryClient.prefetchInfiniteQuery({
+// 		queryKey: ["posts"],
+// 		queryFn: async ({ pageParam = 0 }) => {
+// 			const res = await fetch("/api/v1/posts", {
+// 				method: "POST",
+// 				body: JSON.stringify({ pageParam }),
+// 				headers: {
+// 					"Content-Type": "application/json",
+// 				},
+// 			});
+// 			return res.json();
+// 		},
+// 		initialPageParam: 0,
+// 	});
+
+// 	return { data };
+// }
+
+// export async function clientLoader({ serverLoader, params }: Route.ClientLoaderArgs) {
+// 	const cachedData = queryClient.getQueryData(["posts"]);
+// 	const data = cachedData ?? (await serverLoader());
+
+// 	return data;
+// }
+
+interface PostProps {
+	username: string;
+	title: string;
+	content: string;
+	likes: number;
+	comments: number;
+	createdAt: Date | number;
+}
+
+export function HydrateFallback() {
+	return <div>Loading...</div>;
+}
+
+export default function Index({ loaderData }: Route.ComponentProps) {
 	const inViewportRef = React.useRef(null);
-	const fetchProjects = async ({ pageParam: pageParam = 0 }) => {
-		const res = await fetch("/api/v1/projects", {
+
+	const fetchPosts = async ({ pageParam: pageParam = 0 }) => {
+		const res = await fetch("/api/v1/posts", {
 			method: "POST",
 			body: JSON.stringify({ pageParam }),
 			headers: {
 				"Content-Type": "application/json",
 			},
 		});
+
 		return res.json();
 	};
 
-	const { data, error, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage, status } = useInfiniteQuery({
-		queryKey: ["projects"],
-		queryFn: fetchProjects,
+	const {
+		data: posts,
+		error,
+		fetchNextPage,
+		hasNextPage,
+		isFetching,
+		isFetchingNextPage,
+		status,
+	} = useInfiniteQuery({
+		queryKey: ["posts"],
+		queryFn: fetchPosts,
 		initialPageParam: 0,
 		getNextPageParam: (lastPage, pages) => lastPage.nextCursor,
 	});
@@ -46,29 +96,30 @@ export default function Index() {
 	) : status === "error" ? (
 		<p>Error: {error.message}</p>
 	) : (
-		<>
-			<h1>Welcome</h1>
-			{/* <div>
-				{data?.pages.map((group, i) => (
-					<React.Fragment key={i}>
-						{group.data.map((project: any) => (
-							<p key={project.id}>{project.name}</p>
-						))}
-					</React.Fragment>
-				))}
-				{hasNextPage && (
-					<div ref={inViewportRef}>
-						{isFetchingNextPage && (
-							<p>Loading more...</p>
-						)}
-						{!isFetchingNextPage && (
-							<button onClick={() => fetchNextPage()}>
-								{hasNextPage ? 'Load More' : 'Nothing more to load'}
-							</button>
-						)}
-					</div>
-				)}
-			</div> */}
-		</>
+		<div className="flex h-full w-full flex-col items-center justify-start max-md:w-screen">
+			<div className="overflow-hidden flex w-full max-w-5xl flex-col gap-1 px-10 pt-8 max-sm:px-4">
+				{/* <div>
+					{data?.pages.map((group, i) => (
+						<React.Fragment key={i}>
+							{group.data.map((project: any) => (
+								<p key={project.id}>{project.name}</p>
+							))}
+						</React.Fragment>
+					))}
+					{hasNextPage && (
+						<div ref={inViewportRef}>
+							{isFetchingNextPage && (
+								<p>Loading more...</p>
+							)}
+							{!isFetchingNextPage && (
+								<button onClick={() => fetchNextPage()}>
+									{hasNextPage ? 'Load More' : 'Nothing more to load'}
+								</button>
+							)}
+						</div>
+					)}
+				</div> */}
+			</div>
+		</div>
 	);
 }
