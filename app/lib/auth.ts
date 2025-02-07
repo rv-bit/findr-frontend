b051a94131ac0094c77d79ef6c1c8db809a7974b
Robert Vilcu <robertvilcu89@gmail.com>
2025-02-07 14:47:49 +0000

    feat: add more plugins

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,9 +1,9 @@
-import { adminClient, emailOTPClient, twoFactorClient, usernameClient } from "better-auth/client/plugins";
+import { adminClient, emailOTPClient, multiSessionClient, twoFactorClient, usernameClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
 	baseURL: import.meta.env.VITE_API_URL,
-	plugins: [twoFactorClient(), usernameClient(), emailOTPClient(), adminClient()],
+	plugins: [twoFactorClient(), usernameClient(), emailOTPClient(), adminClient(), multiSessionClient()],
 });
 
 export type Session = typeof authClient.$Infer.Session;
