eca6c90b90259714933d26ae2358537c08342ec6
Robert Vilcu <robertvilcu89@gmail.com>
2025-02-11 15:12:03 +0000

    fix: layout text shifting

--- app/routes/auth/verifications/verify-email.tsx
+++ app/routes/auth/verifications/verify-email.tsx
@@ -66,11 +66,20 @@ export default function Index() {
 		}
 	}, [searchParams]);
 
+	if (loading) {
+		return (
+			<div className="flex h-full w-full flex-col items-center justify-center gap-6 px-2">
+				<div className="flex w-full max-w-lg flex-col gap-6">
+					<h1 className="text-center text-xl font-semibold text-neutral-500 dark:text-neutral-400">Loading...</h1>
+				</div>
+			</div>
+		);
+	}
+
 	return (
 		<div className="flex h-full w-full flex-col items-center justify-center gap-6 px-2">
 			<div className="flex w-full max-w-lg flex-col gap-6">
-				{loading && <h1 className="text-center text-xl font-semibold text-neutral-500 dark:text-neutral-400">Loading...</h1>}
-				{!loading && verified ? (
+				{verified ? (
 					<h1 className="text-center text-xl font-semibold text-neutral-500 dark:text-neutral-400">Email has been verified, you can now close this page</h1>
 				) : (
 					<h1 className="text-center text-xl font-semibold text-neutral-500 dark:text-neutral-400">Failed to verify email</h1>
