48f19866d880d5d2802f3b87119e171110cc3971
Robert Vilcu <robertvilcu89@gmail.com>
2025-03-04 13:29:25 +0000

    chore: changed options

--- app/lib/query/query-client.ts
+++ app/lib/query/query-client.ts
@@ -4,7 +4,6 @@ export const queryClient = new QueryClient({
 	defaultOptions: {
 		queries: {
 			staleTime: 1000 * 60 * 1, // 1 minute
-			retry: false,
 			refetchOnWindowFocus: true,
 			refetchOnReconnect: true,
 		},
