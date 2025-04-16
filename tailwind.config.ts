eef208abdaf3701b47ad3e1c653b9d1eb9aa7b20
Robert Vilcu <robertvilcu89@gmail.com>
2025-04-16 13:13:56 +0100

    chore: refactor font family theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,10 @@
 import type { Config } from "tailwindcss";
 const plugin = require("tailwindcss/plugin");
-const { fontFamily } = require("tailwindcss/defaultTheme");
 
 export default {
 	content: ["./app/**/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		extend: {
-			fontFamily: {
-				sans: ['"Segoe UI"', "Roboto", "sans-serif", ...fontFamily.sans],
-				"bricolage-grotesque": ["Bricolage Grotesque", "sans-serif"],
-			},
 			borderRadius: {
 				lg: "var(--radius)",
 				md: "calc(var(--radius) - 2px)",
