31f79ee7bcc2eac9170aa978ab58eb65ed98f7cd
Robert Vilcu <robertvilcu89@gmail.com>
2025-04-21 19:30:01 +0100

    fix: sheet warning about title and desc

--- app/components/ui/sidebar.tsx
+++ app/components/ui/sidebar.tsx
@@ -10,7 +10,7 @@ import { cn } from "~/lib/utils";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Separator } from "~/components/ui/separator";
-import { Sheet, SheetContent } from "~/components/ui/sheet";
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "~/components/ui/sheet";
 import { Skeleton } from "~/components/ui/skeleton";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "~/components/ui/tooltip";
 
@@ -163,6 +163,14 @@ const Sidebar = React.forwardRef<
 	if (isTablet) {
 		return (
 			<Sheet open={openTablet} onOpenChange={setOpenTablet} {...props}>
+				<SheetHeader className="hidden">
+					<SheetTitle className="text-sidebar-foreground">
+						<SidebarTrigger />
+					</SheetTitle>
+					<SheetDescription className="text-sidebar-foreground">
+						<SidebarInput placeholder="Search..." />
+					</SheetDescription>
+				</SheetHeader>
 				<SheetContent
 					data-sidebar="sidebar"
 					data-mobile="true"
