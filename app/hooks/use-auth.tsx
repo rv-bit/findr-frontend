31f9085f1f3a0f0af88b641dce89ee095aaffb97
Robert Vilcu <robertvilcu89@gmail.com>
2025-04-23 02:50:52 +0100

    chore: remove use auth hooks

--- app/hooks/use-auth.tsx
+++
@@ -1,4 +0,0 @@
-import { createAuthHooks } from "@daveyplate/better-auth-tanstack";
-import { authClient } from "~/lib/auth";
-
-export const { useSession, usePrefetchSession, useListAccounts, useListSessions, useRevokeOtherSessions, useRevokeSession, useRevokeSessions } = createAuthHooks(authClient);
