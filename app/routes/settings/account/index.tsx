3858198f9f78547aebbe1cf98ca0ca59bfd0dbb0
Robert Vilcu <robertvilcu89@gmail.com>
2025-05-01 02:29:51 +0100

    chore: moved location for dialog

--- app/routes/settings/account/index.tsx
+++ app/routes/settings/account/index.tsx
@@ -18,7 +18,7 @@ import { Button } from "~/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "~/components/ui/form";
 import { Input } from "~/components/ui/input";
 
-import WarningComponent from "~/components/warning-dialog";
+import WarningComponent from "~/components/ui/warning-dialog";
 
 import { ChevronRight, TriangleAlert, type LucideIcon } from "lucide-react";
 
