8448025f52d601dc7c3e52519cec10a6234da67b
Robert Vilcu <robertvilcu89@gmail.com>
2025-05-03 00:47:42 +0100

    chore: changed default query options

--- app/lib/query/query-client.ts
+++ app/lib/query/query-client.ts
@@ -19,9 +19,9 @@ function createIDBPersister(idbValidKey: IDBValidKey = "reactQuery") {
 const queryClient = new QueryClient({
 	defaultOptions: {
 		queries: {
-			gcTime: 1000 * 60 * 60 * 24, // 24 hours
+			gcTime: 1000 * 60 * 60 * 24, // 1 day
 			staleTime: 1000 * 60 * 1, // 1 minute
-			refetchInterval: 1000 * 30, // 30 seconds
+			// refetchInterval: 1000 * 30, // 30 seconds
 			refetchOnWindowFocus: true,
 			refetchOnReconnect: true,
 		},
