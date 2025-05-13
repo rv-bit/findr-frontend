74541a3020c37326340d2445bd1a605ca587497e
Robert Vilcu <robertvilcu89@gmail.com>
2025-05-13 15:15:07 +0100

    chore: refactor layout for home posts

--- app/routes/home/components/posts.card.tsx
+++ app/routes/home/components/posts.card.tsx
@@ -26,6 +26,7 @@ import { useMutatePostVote } from "~/hooks/useMutatePostVote";
 
 import { BellDot, Bookmark, Ellipsis, Flag, MessageCircle, Pencil, ThumbsDown, ThumbsUp, Trash2, type LucideIcon } from "lucide-react";
 
+import HoverCardUser from "~/components/hover.card.user";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Button } from "~/components/ui/button";
 import {
@@ -165,198 +166,212 @@ const PostsCard = React.memo(
 		);
 
 		return (
-			<Link to={`/post/${data.id}`}>
-				<article
-					className={cn(
-						"flex h-auto max-h-96 min-h-28 w-full flex-col justify-between gap-3 rounded-xl px-4 py-2 hover:bg-sidebar-foreground/10 dark:hover:bg-sidebar-accent/50",
-						className,
-					)}
-				>
-					<span className="flex items-center justify-between gap-1">
-						<section className="flex items-center justify-start gap-1">
-							<span className="flex items-center justify-center gap-2">
-								<Avatar className="size-6 rounded-full">
-									<AvatarImage loading="lazy" src={data.user.image ?? ""} alt={data.user.username} />
-									<AvatarFallback className="rounded-lg bg-sidebar-foreground/50">
-										{data.user.username
-											?.split(" ")
-											.map((name) => name[0])
-											.join("")}
-									</AvatarFallback>
-								</Avatar>
-								<h1 className="text-sm text-black dark:text-white">{data.user.username}</h1>
-							</span>
-							<span className="my-0 inline-block text-[#333a3e] dark:text-[#333a3e]">•</span>
-							<h2 className="text-xs text-black dark:text-white">{time}</h2>
-						</section>
-						<DropdownMenu>
-							<DropdownMenuTrigger asChild>
-								<Button
+			<article
+				className={cn(
+					"relative flex h-auto max-h-96 min-h-28 w-full flex-col justify-between gap-1.5 rounded-xl px-4 py-2 hover:bg-sidebar-foreground/10 dark:hover:bg-sidebar-accent/50",
+					className,
+				)}
+			>
+				<Link to={`/post/${data.id}`} className="absolute inset-0" />
+
+				<span className="flex items-center justify-between gap-1">
+					<section className="flex w-fit items-center justify-start gap-1">
+						<span className="relative flex items-center justify-center gap-2">
+							<HoverCardUser username={data.user.username}>
+								<Link
 									onClick={(e) => {
+										e.preventDefault();
 										e.stopPropagation();
 									}}
-									variant={"link"}
-									size="lg"
-									className="flex h-auto items-center justify-end rounded-full p-1 hover:bg-sidebar-foreground/20 hover:text-white focus-visible:border-0 focus-visible:ring-0 dark:hover:bg-sidebar-accent dark:focus-visible:border-0 dark:focus-visible:ring-0"
+									to={`/users/${data.user.username}`}
+									className="group flex h-fit w-fit cursor-pointer items-center justify-start gap-2 p-0 hover:no-underline"
 								>
-									<Ellipsis />
-								</Button>
-							</DropdownMenuTrigger>
-							<DropdownMenuContent
-								className="mt-3.5 w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg border-none dark:bg-modal"
-								side={"bottom"}
-								align="end"
-								sideOffset={4}
+									<Avatar className="size-6 rounded-full">
+										<AvatarImage loading="lazy" src={data.user.image ?? ""} alt={data.user.username} />
+										<AvatarFallback className="rounded-lg bg-sidebar-foreground/50">
+											{data.user.username
+												?.split(" ")
+												.map((name) => name[0])
+												.join("")}
+										</AvatarFallback>
+									</Avatar>
+
+									<h1 className="text-sm break-all text-black dark:text-white">{data.user.username}</h1>
+								</Link>
+							</HoverCardUser>
+						</span>
+						<span className="my-0 inline-block text-[#333a3e] dark:text-[#333a3e]">•</span>
+						<h2 className="text-xs text-black dark:text-white">{time}</h2>
+					</section>
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<Button
+								onClick={(e) => {
+									e.stopPropagation();
+								}}
+								variant={"link"}
+								size="lg"
+								className="relative flex h-auto items-center justify-end rounded-full p-1 hover:bg-sidebar-foreground/20 hover:text-white focus-visible:border-0 focus-visible:ring-0 dark:hover:bg-sidebar-accent dark:focus-visible:border-0 dark:focus-visible:ring-0"
 							>
-								{dropDownActions.map((item, index) =>
-									item.items ? (
-										<DropdownMenuGroup key={item.title}>
-											{item.items?.map(
-												(action) =>
-													action.show && (
-														<DropdownMenuItem
-															key={action.title}
-															onClick={(e) => {
-																e.preventDefault();
-
-																if (action.onClick) {
-																	action.onClick();
-																}
-															}}
-															className="group h-auto w-full px-3 py-2 text-left hover:cursor-pointer"
-														>
-															{action.component ? (
-																<action.component />
-															) : (
-																<span className="flex w-full items-center justify-start gap-1 opacity-80 group-hover:opacity-100">
-																	{action.icon && <action.icon />}
-																	<h1>{action.title}</h1>
-																</span>
-															)}
-														</DropdownMenuItem>
-													),
-											)}
-
-											{dropDownActions.length - 1 !== index && dropDownActions[index + 1].show && <DropdownMenuSeparator />}
-										</DropdownMenuGroup>
-									) : (
-										item.show && (
-											<DropdownMenuGroup key={item.title}>
-												<DropdownMenuItem
-													onClick={(e) => {
-														e.preventDefault();
-
-														if (item.onClick) {
-															item.onClick();
-														}
-													}}
-													className="group w-full px-3 py-2 text-left hover:cursor-pointer"
-												>
-													{item.component ? (
-														<item.component />
-													) : (
-														<span className="flex w-full items-center justify-start gap-1 opacity-80 group-hover:opacity-100">
-															{item.icon && <item.icon />}
-															<h1>{item.title}</h1>
-														</span>
-													)}
-												</DropdownMenuItem>
-
-												{dropDownActions.length - 1 !== index && <DropdownMenuSeparator />}
-											</DropdownMenuGroup>
-										)
-									),
-								)}
-							</DropdownMenuContent>
-						</DropdownMenu>
-					</span>
-
-					<span className="flex h-full flex-col items-start justify-start gap-1 overflow-hidden text-ellipsis">
-						<h1 className="w-full text-lg font-bold break-all text-black dark:text-white">{data.title}</h1>
-
-						<ClientOnly>
-							{() => (
-								<MDXEditor
-									markdown={JSON.parse(data.content)}
-									plugins={[
-										quotePlugin(),
-										listsPlugin(),
-										headingsPlugin({
-											allowedHeadingLevels: [1, 2, 3],
-										}),
-										quotePlugin(),
-										thematicBreakPlugin(),
-										markdownShortcutPlugin(),
-										linkPlugin(),
-										linkDialogPlugin(),
-									]}
-									className="w-full overflow-hidden text-ellipsis"
-									contentEditableClassName="text-ellipsis line-clamp-10 text-gray-500 dark:text-gray-400 w-full"
-									readOnly={true}
-								/>
-							)}
-						</ClientOnly>
-					</span>
-
-					<span className="flex items-start justify-start gap-2">
-						<span
-							className={cn("flex w-fit items-center justify-between gap-1 rounded-3xl bg-[#E5EBEE] dark:bg-sidebar-accent", {
-								"bg-red-400/85 dark:bg-red-400/85": data.downvoted,
-								"bg-primary-400 dark:bg-primary-400": data.upvoted,
-							})}
+								<Ellipsis />
+							</Button>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent
+							className="mt-3.5 w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg border-none dark:bg-modal"
+							side={"bottom"}
+							align="end"
+							sideOffset={4}
 						>
-							<span className="flex items-center justify-center">
-								<Button
-									onClick={handleUpvote}
-									className={cn(
-										"group flex items-center justify-center gap-1 rounded-full bg-transparent p-2 px-3 text-black/70 shadow-none hover:bg-[#75858f]/20 dark:bg-transparent dark:text-white dark:hover:bg-[#333a3e] [&_svg]:size-4",
-										{
-											"text-white hover:bg-[#75858f]/50 dark:hover:bg-[#333a3e]/40": data.upvoted || data.downvoted,
-										},
-									)}
-								>
-									<ThumbsUp
-										className={cn("group-hover:text-primary-400", {
-											"group-hover:text-white": data.upvoted || data.downvoted,
-										})}
-									/>
-								</Button>
-							</span>
+							{dropDownActions.map((item) =>
+								item.items ? (
+									<DropdownMenuGroup key={item.title}>
+										{item.items?.map(
+											(action) =>
+												action.show && (
+													<DropdownMenuItem
+														key={action.title}
+														onClick={(e) => {
+															e.preventDefault();
 
-							<h1
-								className={cn("text-sm text-black/70 dark:text-white", {
-									"text-white": data.upvoted || data.downvoted,
-								})}
+															if (action.onClick) {
+																action.onClick();
+															}
+														}}
+														className="group h-auto w-full px-3 py-2 text-left hover:cursor-pointer"
+													>
+														{action.component ? (
+															<action.component />
+														) : (
+															<span className="flex w-full items-center justify-start gap-1 opacity-80 group-hover:opacity-100">
+																{action.icon && <action.icon />}
+																<h1>{action.title}</h1>
+															</span>
+														)}
+													</DropdownMenuItem>
+												),
+										)}
+
+										{dropDownActions.length - 1 !== dropDownActions.indexOf(item) && <DropdownMenuSeparator />}
+									</DropdownMenuGroup>
+								) : (
+									item.show && (
+										<DropdownMenuGroup key={item.title}>
+											<DropdownMenuItem
+												onClick={(e) => {
+													e.preventDefault();
+
+													if (item.onClick) {
+														item.onClick();
+													}
+												}}
+												className="group w-full px-3 py-2 text-left hover:cursor-pointer"
+											>
+												{item.component ? (
+													<item.component />
+												) : (
+													<span className="flex w-full items-center justify-start gap-1 opacity-80 group-hover:opacity-100">
+														{item.icon && <item.icon />}
+														<h1>{item.title}</h1>
+													</span>
+												)}
+											</DropdownMenuItem>
+
+											{dropDownActions.length - 1 !== dropDownActions.indexOf(item) && <DropdownMenuSeparator />}
+										</DropdownMenuGroup>
+									)
+								),
+							)}
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</span>
+
+				<span className="flex h-full flex-col items-start justify-start -space-y-0.5 overflow-hidden text-ellipsis">
+					<h1 className="w-full text-lg font-bold break-all text-black dark:text-white">{data.title}</h1>
+					<ClientOnly>
+						{() => (
+							<MDXEditor
+								markdown={JSON.parse(data.content)}
+								plugins={[
+									quotePlugin(),
+									listsPlugin(),
+									headingsPlugin({
+										allowedHeadingLevels: [1, 2, 3],
+									}),
+									quotePlugin(),
+									thematicBreakPlugin(),
+									markdownShortcutPlugin(),
+									linkPlugin(),
+									linkDialogPlugin(),
+								]}
+								className="w-full overflow-hidden text-ellipsis"
+								contentEditableClassName="text-ellipsis text-sm line-clamp-10 text-gray-500 dark:text-gray-400 w-full"
+								readOnly={true}
+							/>
+						)}
+					</ClientOnly>
+				</span>
+
+				<span className="relative mt-1 flex items-start justify-start gap-2">
+					<span
+						className={cn("flex w-fit items-center justify-between gap-1 rounded-3xl bg-[#E5EBEE] dark:bg-sidebar-accent", {
+							"bg-red-400/85 dark:bg-red-400/85": data.downvoted,
+							"bg-primary-400 dark:bg-primary-400": data.upvoted,
+						})}
+					>
+						<span className="flex items-center justify-center">
+							<Button
+								onClick={handleUpvote}
+								className={cn(
+									"group flex size-9 items-center justify-center gap-1 rounded-full bg-transparent p-0 text-black/70 shadow-none hover:bg-[#75858f]/20 dark:bg-transparent dark:text-white dark:hover:bg-[#333a3e] [&_svg]:size-4",
+									{
+										"text-white hover:bg-[#75858f]/50 dark:hover:bg-[#333a3e]/40": data.upvoted || data.downvoted,
+									},
+								)}
 							>
-								{data.likesCount}
-							</h1>
-
-							<span className="flex items-center justify-center">
-								<Button
-									onClick={handleDownvote}
-									className={cn(
-										"group flex items-center justify-center gap-1 rounded-full bg-transparent p-2 px-3 text-black/70 shadow-none hover:bg-[#75858f]/20 dark:bg-transparent dark:text-white dark:hover:bg-[#333a3e] [&_svg]:size-4",
-										{
-											"text-white hover:bg-[#75858f]/50 dark:hover:bg-[#333a3e]/40": data.upvoted || data.downvoted,
-										},
-									)}
-								>
-									<ThumbsDown
-										className={cn("group-hover:text-red-400/85", {
-											"group-hover:text-white": data.upvoted || data.downvoted,
-										})}
-									/>
-								</Button>
-							</span>
+								<ThumbsUp
+									className={cn("group-hover:text-primary-400", {
+										"group-hover:text-white": data.upvoted || data.downvoted,
+									})}
+								/>
+							</Button>
 						</span>
 
-						<Button className="flex h-9 w-fit items-center justify-start gap-1 rounded-3xl bg-[#E5EBEE] px-3 py-2 text-black hover:bg-[#75858f]/20 dark:bg-sidebar-accent dark:text-white dark:hover:bg-[#333a3e] [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0">
-							<MessageCircle />
-							<span className="text-sm text-black dark:text-white">{data.commentsCount}</span>
-						</Button>
+						<h1
+							className={cn("text-sm text-black/70 dark:text-white", {
+								"text-white": data.upvoted || data.downvoted,
+							})}
+						>
+							{data.likesCount}
+						</h1>
+
+						<span className="flex items-center justify-center">
+							<Button
+								onClick={handleDownvote}
+								className={cn(
+									"group flex size-9 items-center justify-center gap-1 rounded-full bg-transparent p-0 text-black/70 shadow-none hover:bg-[#75858f]/20 dark:bg-transparent dark:text-white dark:hover:bg-[#333a3e] [&_svg]:size-4",
+									{
+										"text-white hover:bg-[#75858f]/50 dark:hover:bg-[#333a3e]/40": data.upvoted || data.downvoted,
+									},
+								)}
+							>
+								<ThumbsDown
+									className={cn("group-hover:text-red-400/85", {
+										"group-hover:text-white": data.upvoted || data.downvoted,
+									})}
+								/>
+							</Button>
+						</span>
 					</span>
-				</article>
-			</Link>
+
+					<Link
+						to={`/post/${data.id}`}
+						className="flex h-9 w-fit items-center justify-start gap-1 rounded-3xl bg-[#E5EBEE] px-3 py-2 text-black hover:bg-[#75858f]/20 dark:bg-sidebar-accent dark:text-white dark:hover:bg-[#333a3e] [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+					>
+						<MessageCircle />
+						<span className="text-sm text-black dark:text-white">{data.commentsCount}</span>
+					</Link>
+				</span>
+			</article>
 		);
 	},
 );
