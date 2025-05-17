073b89fbda79372de7ce2cd7fdf690ee18bde81d
Robert Vilcu <robertvilcu89@gmail.com>
2025-05-17 23:40:30 +0200

    style: minor style changes

--- app/components/ui/textarea-label.tsx
+++ app/components/ui/textarea-label.tsx
@@ -10,14 +10,14 @@ export default function TextareaLabel({ className, ...props }: React.ComponentPr
 				className="text-muted-foreground group-focus-within:text-foreground has-[+input:not(:placeholder-shown)]:text-foreground absolute top-1/2 left-2 block origin-top -translate-y-1/2 cursor-text px-1 text-sm transition-all group-focus-within:pointer-events-none group-focus-within:top-4 group-focus-within:left-2 group-focus-within:cursor-default group-focus-within:text-xs group-focus-within:font-medium has-[+input:not(:placeholder-shown)]:pointer-events-none has-[+input:not(:placeholder-shown)]:top-4 has-[+input:not(:placeholder-shown)]:left-2 has-[+input:not(:placeholder-shown)]:cursor-default has-[+input:not(:placeholder-shown)]:text-xs has-[+input:not(:placeholder-shown)]:font-medium"
 			>
 				<span
-					className={cn("relative capitalize", {
+					className={cn("relative text-black capitalize dark:text-white", {
 						"after:text-red-500 after:content-['*']": props.required,
 					})}
 				>
 					{props.name}
 				</span>
 			</label>
-			<Textarea id={props.id} className={cn("pb-1 group-focus-within:pt-5.5", className)} placeholder="" {...props} />
+			<Textarea id={props.id} className={cn("py-0 group-focus-within:pt-5.5", className)} placeholder="" {...props} />
 		</div>
 	);
 }
